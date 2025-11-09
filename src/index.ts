732d1080ea2cb6c8f9d85dbeda147bde26c808d4
TonNom <ton.email@example.com>
2025-11-09 13:42:46 +0100

    Initial commit TypeScript simple

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// src/index.ts
+console.log("🚀 Hello GitHub + TypeScript!");
+
+// Petite fonction utilitaire
+function greet(name: string): string {
+    return `Bonjour, ${name}! 👋`;
+}
+
+const user = "Karim";
+console.log(greet(user));
